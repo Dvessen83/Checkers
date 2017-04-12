a539aa078a132fc30a0c1f585352dbd4edb973d9
SarahPAms <Sarah.L.Polan@gmail.com>
2017-04-12 20:10:07 +0200

    multi dimensional hook update

--- src/services/board/hooks/createBoard.js
+++ src/services/board/hooks/createBoard.js
@@ -9,7 +9,7 @@ return function(hook){
   board.arr.push([1, 2][3, 4][5, 6]);
   // now you have to tell mongoose that the value has changed
       // because with Mixed types it's not done automatically...
-      board.markModified('board');
-  board.save();
+      hook.data.board.markModified('board');
+  hook.data.save();
   }
 }
